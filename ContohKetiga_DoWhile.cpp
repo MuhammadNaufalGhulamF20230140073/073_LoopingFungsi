819b2ee60bdca011f64d508126102b14318c245c
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-03-18 14:59:38 +0700

    Perulangan Do while

---
+++ ContohKetiga_DoWhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char pilihan;
+
+    do{
+        cout << " Berangkat Ambil Takjil" << endl;
+        cout << " Antri Takjil " << endl;
+        cout << " Ambil Takjil" << endl;
+        cout << " Apakah Mau Antri Kembali?" << endl;
+        cin >> pilihan;
+    }while (pilihan == 'y' || pilihan == 'y');
+    
+    
+}
